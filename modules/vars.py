90a0501d1fab0faf21e2a03d0818163036bf665c
Cjxtpro1 <145145809+Cjxtpro1@users.noreply.github.com>
2024-10-28 15:41:28 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "0000"
-api_hash = "0000"
-bot_token = "0000"
+api_id = "22789024"
+api_hash = "05dd73c56053828044cb71216cdfd0cc"
+bot_token = "6855450527:AAE_ij9agZiig5QdZQl2bs3ddPtSEoUSOKs"
